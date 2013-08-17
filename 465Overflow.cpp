203253191f4c27f67a9a7b05574868c8de20ad0b
bhatpraveen <bhatpraveen@gmail.com>
2013-08-17 15:23:10 +0530

    This is the initial version that uses strings. The char array version was used only because of multiple WAs. I suspected that it was due to leading spaces in numbers! However, the fix was needed for leading zeroes, not leading spaces! Smaller number with leading zeroes that made it larger than 10 digits was causing WA. This strings version is much faster.

--- 465Overflow.cpp
+++ 465Overflow.cpp
@@ -1,7 +1,5 @@
 /* UVa 465 - Overflow http://uva.onlinejudge.org/external/4/465.pdf */
 #include <iostream>
-#include <cstdio>
-#include <string.h>
 using namespace std;
 
 static const char FNTB[] = "first number too big";
@@ -9,23 +7,15 @@ static const char SNTB[] = "second number too big";
 static const char RTB[] = "result too big";
 static const unsigned int HIGHEST = 2147483647;
 
-//static inline int GetNumber(string &s) {
-static inline int GetNumber(char s[]) {
+static inline int GetNumber(string &s) {
     //Returns number if not too big, else returns -1
     long long int n = 0;
-    int leading = 0;
-
-    while(s[leading] == ' ')
-        leading++;
-
-    //if(s.size() > 10) return -1;
-    //if(s.strlen() > 10) return -1;
+    //if(s.size() > 10) return -1; //This was causing WA, since there could be many leading zeroes!
 
     //Valid integer?
-    //for(int i = 0; i < s.size(); ++i) {
-    for(int i = leading; i < strlen(s) && s[i] != ' '; ++i) {
+    for(int i = 0; i < s.size(); ++i) {
         n = n*10 + (s[i]&0xf);
-        if(n > HIGHEST) return -1;
+        if(n > HIGHEST) return -1; //Fix for leading zeroes, check after each digit is added
     }
 
     if(n > HIGHEST) return -1;
@@ -34,14 +24,11 @@ static inline int GetNumber(char s[]) {
 
 int main() {
     int fn, sn;
-    //string fns, sns;
-    char fns[10000], sns[10000];
-    //char op;
-    char op[2];
+    string fns, sns;
+    char op;
     bool fntb, sntb, rtb;
 
-    //while(cin >> fns >> op >> sns) {
-    while(scanf("%s %s %s", &fns, &op, &sns) == 3) {
+    while(cin >> fns >> op >> sns) {
         fntb = sntb = rtb = false;
 
         fn = GetNumber(fns);
@@ -51,8 +38,7 @@ int main() {
         if(sn < 0) sntb = rtb = true;
         //cout << endl<< endl << "IS THIS RIGHT? " << fn << " " << sn << endl<< endl;
 
-        //if (op == '+') {
-        if (op[0] == '+') {
+        if (op == '+') {
             if(!fntb && !sntb)
                 if((HIGHEST - fn) < sn) rtb = true;
         }
