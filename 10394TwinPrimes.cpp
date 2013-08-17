22c622ff53ddc010d1f66d556b08be24ffaae6f4
bhatpraveen <bhatpraveen@gmail.com>
2013-08-17 20:15:11 +0530

    Runtime improvements made: --Replaced bool with bitset (although this will likely improve performance less). --Changed unnecessary multiple mod calculations. --Sieve of Atkin improved by seperating the 4x^2 + y^2 loop and running it half the root number of times instead of root times.

--- 10394TwinPrimes.cpp
+++ 10394TwinPrimes.cpp
@@ -1,43 +1,50 @@
 /* UVa 10394 Twin Primes */
 #include <cstdio>
+#include <bitset>
 using namespace std;
 
 const int HIGHEST = 20000000;
-bool primes[HIGHEST];
+//bool primes[HIGHEST];
+bitset<HIGHEST> primes;
 int twins[100001];
 int si = 0;
 
-inline void SieveOfAtkin(bool primes[], const int &largest) {
+inline void SieveOfAtkin(const int &largest) {
     int square;
 
     for (int i = 2; i < largest; ++i) primes[i] = false;
-    primes[0] = false;
-    primes[1] = false;
-    primes[2] = true;
-    primes[3] = true;
+    primes.set(0, false);
+    primes.set(1, false);
+    primes.set(2, true);
+    primes.set(3, true);
     //Sieve Of Atkin
+    int n, nmod12;
+    for (int x = 1; x <= 2236; ++x) { //x till root/2
+        for (int y = 1; y <= 4472; y += 2) { //odd y
+            n = 4*x*x + y*y;
+            nmod12 = n%12;
+            if(n < largest && (nmod12 == 1 || nmod12 == 5)) primes.flip(n);
+        }
+    }
+
     for (int x = 1; x <= 4472; ++x) {
         for (int y = 1; y <= 4472; ++y) {
-            int n = 4*x*x + y*y;
-            if(n < largest && (n%12 == 1 || n%12 == 5)) primes[n] = !primes[n];
-
             n = 3*x*x + y*y;
-            if(n < largest && (n%12 == 7)) primes[n] = !primes[n];
+            nmod12 = n%12;
+            if(n < largest && (nmod12 == 7)) primes.flip(n);
 
             n = 3*x*x - y*y;
-            if(x > y && n < largest && (n%12 == 11)) primes[n] = !primes[n];
+            nmod12 = n%12;
+            if(x > y && n < largest && (nmod12 == 11)) primes.flip(n);
         }
     }
 
     //Eliminate by Sieve
     for (int i = 5; i <= 4472; ++i) {
-        if(primes[i]) {
+        if(primes.test(i)) {
             square = i * i;
-            //int m = 1;
-            //for(int j = square; j <= largest; j = square + (m * i)) {
             for(int j = square; j <= largest; j += square) {
                 primes[j] = false;
-                //++m;
             }
         }
     }
@@ -46,7 +53,7 @@ inline void SieveOfAtkin(bool primes[], const int &largest) {
 int main() {
     int s;
     //Generate Primes
-    SieveOfAtkin(primes, HIGHEST);
+    SieveOfAtkin(HIGHEST);
 
     //Mark twins
     //for(int i = 4472; si <= 100000; ++i) {
