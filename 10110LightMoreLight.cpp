75fd10caef3a37703c0953b87ceed207c78c9133
bhatpraveen <bhatpraveen@gmail.com>
2013-08-17 14:44:15 +0530

    Checking if runtime can be improved by not squaring the root and instead checking its floor and ceil values to check if its an integer. (Note: runtime worsened by this, marginally)

--- 10110LightMoreLight.cpp
+++ 10110LightMoreLight.cpp
@@ -4,16 +4,15 @@
 using namespace std;
 
 int main() {
-    unsigned long long int n, root;
+    unsigned long long int n;
+    double root;
     while(scanf("%llu", &n) == 1) {
         if(n == 0)
-            break;
-        if(n < 0) printf("no\n");
-        else {
-            root = (unsigned long long int)sqrt((double)n);
-            if(root * root == n) printf("yes\n");
-            else printf("no\n");
-        }
+            return 0;
+        root = sqrt((double)n);
+        //if(root * root == n) printf("yes\n");
+        if(floor(root) == ceil(root)) printf("yes\n");
+        else printf("no\n");
     }
     return 0;
 }
